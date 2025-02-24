113aa1622d82888d21607569a2301cce4fab9f76
maymay911 <estereka09@gmail.com>
2025-02-24 13:36:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ window.addEventListener('load', () => {
 
 const messages = [
     {
-        text: "plss mau dong",
+        text: "plss mau dong sahabat",
         image: "https://i.pinimg.com/736x/1e/7c/6f/1e7c6ff7a0c26f7eaefbce683656193d.jpg"
     },
     {
